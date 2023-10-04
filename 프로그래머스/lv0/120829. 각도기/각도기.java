8a3897a6c46db0f390588b27433b410a796c49f4
Jensuchan <143565449+Jensuchan@users.noreply.github.com>
2023-10-04 22:57:46 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int angle) {
+        int answer = 1;
+        if(angle == 180){
+            return 4;
+        }else if(angle>90){
+            return 3;
+        }else if(angle == 90){
+            return 2;
+        }
+        return answer;
+    }
+}
